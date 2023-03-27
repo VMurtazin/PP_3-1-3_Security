0b40cecabc46c6940c97421722de717230960c98
VMurtazin <obdorsk@gmail.com>
2023-03-27 22:50:37 +0500

    startFinalVersion

---
+++ src/main/java/ru/murtazin/PP_313_Security/Pp313SecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.murtazin.PP_313_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp313SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp313SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/murtazin/PP_313_Security/Pp313SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.murtazin.PP_313_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp313SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
