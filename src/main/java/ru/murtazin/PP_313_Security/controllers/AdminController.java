e6685fd3ce3d8dc076e6dd7396594b102d089399
VMurtazin <obdorsk@gmail.com>
2023-04-02 20:30:54 +0500

    addAdminController

---
+++ src/main/java/ru/murtazin/PP_313_Security/controllers/AdminController.java
@@ -0,0 +1,81 @@
+package ru.murtazin.PP_313_Security.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.murtazin.PP_313_Security.model.User;
+import ru.murtazin.PP_313_Security.service.RoleService;
+import ru.murtazin.PP_313_Security.service.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final RoleService roleService;
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String adminPage(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "/admin";
+    }
+
+    @GetMapping("/new")
+    public String userCreatePage(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("roles", roleService.getRoles());
+        return "new";
+    }
+
+    @PostMapping("/createNew")
+    public String createUser(ModelMap modelMap, @ModelAttribute("user") @Valid User user,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            modelMap.addAttribute("roles", roleService.getRoles());
+            return "new";
+        }
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping(value = "edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        model.addAttribute("roles", roleService.getRoles());
+        return "edit";
+    }
+
+    @PatchMapping(value = "edit/{id}")
+    public String updateUser(ModelMap modelMap, @ModelAttribute("user") @Valid User user,
+                             BindingResult bindingResult, @PathVariable("id") Long id) {
+        if (bindingResult.hasErrors()) {
+            modelMap.addAttribute("roles", roleService.getRoles());
+            return "edit";
+        }
+        userService.updateUser(user, id);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.removeUserById(id);
+        return "redirect:/admin";
+    }
+}
